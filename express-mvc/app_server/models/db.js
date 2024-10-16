7d10963838c81658a11c3d619bd854877c72d5a2
Mozza27 <syeirakhaerani@gmail.com>
2024-10-16 09:31:04 +0700

    latihan-mongo

---
+++ express-mvc/app_server/models/db.js
@@ -0,0 +1,20 @@
+let mongoose = require("mongoose");
+let dbURI = "mongodb://localhost:27017/Server Matkul PAW2";
+
+mongoose.connect (dbURI, {
+    //useNewUrlParser: true
+});
+mongoose.connection.on ("connected", () =>{
+    console.log ("connected to MongoDB");
+});
+mongoose.connection.on ("error", (error) =>{
+    console.log("Connection Error: " + error);
+});
+mongoose.connection.on("disconnected",()=>{
+    console.log("Disconnected from MongoDB");
+
+});
+
+
+
+
